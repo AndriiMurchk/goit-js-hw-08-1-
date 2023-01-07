d8e4f6db983c6cc25cc63b113860881fc66528aa
AndriiMurchk <andr.murchik@gmail.com>
2023-01-07 16:27:26 +0200

    edit

--- src/js/exmple.js
+++ src/js/exmple.js
@@ -3,16 +3,14 @@ import throttle from 'lodash.throttle';
 
 
 const feedbackForm = document.querySelector('.feedback-form');
+let formData = {};
 
 fillFeedbackFormFields();
 
 const onFeedbackFormItemChange =  evt => {
-    
-    let persistedForm = localStorage.getItem('feedback-form-state');
-    persistedForm = persistedForm ? JSON.parse(persistedForm) : {};
-    persistedForm[evt.target.name] = evt.target.value;
-    localStorage.setItem('feedback-form-state', JSON.stringify(persistedForm));
-    
+    formData[evt.target.name] = evt.target.value;
+  
+    localStorage.setItem('feedback-form-state', JSON.stringify(formData));
 };
 
 function fillFeedbackFormFields() {
@@ -22,17 +20,20 @@ function fillFeedbackFormFields() {
        
         Object.entries(persistedForm).forEach(([name,value]) => {
            
-        feedbackForm.elements[name].value = value;
+            formData[name] = value;
+           feedbackForm.elements[name].value = value;
     });
 
     }
 }; 
 
 
+
 const onFeedbackFormSubmit = event => {
     event.preventDefault();
 
-    console.log();
+    console.log(formData);
+    formData = {};
     feedbackForm.reset();
     localStorage.removeItem('feedback-form-state');
 }
